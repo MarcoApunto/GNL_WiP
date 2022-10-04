cdaca5dbf7e108bf00972be18a6be1c15d488a2e
marcofs <ferreiras.marcoa@gmail.com>
2022-10-04 12:05:46 +0200

    stucked_on_start

--- test_fcntl.c
+++ test_fcntl.c
@@ -1,41 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   test_fcntl.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: marferre <marferre@student.42madrid.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/03 23:13:43 by marferre          #+#    #+#             */
-/*   Updated: 2022/10/04 09:29:16 by marferre         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+# include <fcntl.h>
+# include <unistd.h>
 #include <stdio.h>
-#include "get_next_line.h"
 
 int main()
 {
 	int fd;
-	char buff[30];
-	int	nbt;
+	char buff[10];
+	ssize_t	nbt;
 
 	fd = open("file.txt", O_RDONLY);
 	if (fd == -1)
 		printf("error\n");
 	else
 	{
-		nbt = read(fd, buff, 30);
-		
+		nbt = read(fd, buff, 5);
 		close(fd);
-		
 		if (!nbt)
-			printf("empty")
+			printf("vacío\n");
 		else
-			while (!(ft_strchr(buff, '\n')))
-				write(1, buff, 1);
+			printf("characters: %d, content: %s \n", (int)nbt, buff);
 	}
 	return (0);
 }
